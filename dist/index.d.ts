a4345c0976f568130933f70a6459732623d07960
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2026-05-17 23:21:09 +0200

    dist: Delete old dist artifacts

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function install(): Promise<string>;
-export declare function checkForModifiedPackages(cargo: string, workspace: string): Promise<boolean>;
-export declare function run(): Promise<void>;
